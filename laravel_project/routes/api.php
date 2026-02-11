8e08f0f2300b45a909d20be33ace789b2360a49d
Ryan Otacan <otacanryan@gmail.com>
2026-02-11 10:00:00 +0800

    CRUD ACTIVITY

--- laravel_project/routes/api.php
+++ laravel_project/routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('users')->group(function () {
+    Route::get('/', [App\Http\Controllers\UserController::class, 'index']);
+    Route::post('/', [App\Http\Controllers\UserController::class, 'store']);
+    Route::get('/{id}', [App\Http\Controllers\UserController::class, 'show']);
+    Route::put('/{id}', [App\Http\Controllers\UserController::class, 'update']);
+    Route::delete('/{id}', [App\Http\Controllers\UserController::class, 'destroy']);
+});
